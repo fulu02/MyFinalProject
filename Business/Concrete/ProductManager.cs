1bd5ab9fb89d3c4df20a678f54bc55cc912567a6
Fatma <Fatma@DESKTOP-Q3FGQH0>
2021-03-28 21:06:02 +0300

    Add project files.

---
+++ Business/Concrete/ProductManager.cs
@@ -0,0 +1,18 @@
+﻿using Business.Abstract;
+using DataAccess.Abstract;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Business.Cocrete
+{
+    public class ProductManager : IProductService
+    {
+        IProductDal _productDal;
+        public ProductManager(IProductDal productDal)
+        {
+            _productDal = productDal;
+        }
+        public List<product> GetAll() => _productDal.GetAll();
+    }
+}
---
+++ ConsoleUI/Program.cs
@@ -0,0 +1,18 @@
+﻿using Business.Cocrete;
+using DataAccess.Concrete.InMemory;
+using System;
+
+namespace ConsoleUI
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ProductManager productManager = new ProductManager(new InMemoryProductDal());
+            foreach (Business.Abstract.product product in ProductManager.GetAll())
+            {
+                Console.WriteLine(product.ProductName);
+            }
+        }
+    }
+}
